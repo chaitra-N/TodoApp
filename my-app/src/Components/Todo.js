9a9de23d8f46f1d94cb86a609c1b28319e61ac6b
Chai <developer.chaitra@gmail.com>
2023-04-10 22:45:43 +1200

    Layout of the app

--- my-app/src/Components/Todo.js
+++ my-app/src/Components/Todo.js
@@ -4,9 +4,26 @@ import React from "react";
 
 const Todo = () => {
   return (
-    <div>
-      <h1>To Do App</h1>
-    </div>
+    <section className=' w-[90vw] max-w-[35rem] mt-32 mx-auto my-0 rounded shadow-[0_5px_15px_rgba(0,0,0,0.1))] transition-all duration-[0.3s] ease-linear p-8 hover:shadow-[0_5px_15px_rgba(0,0,0,0.5)] bg-violet-100'>
+      <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center'>
+        <h3 className='tracking-[0.1rem] capitalize leading-tight text-xl mb-3'>
+          To Do List
+        </h3>
+        <div className='flex justify-center'>
+          <input
+            type='text'
+            className='text-base flex-[1_0_auto] text-[hsl(210,22%,49%)] pl-4 p-1 rounded-tl rounded-bl border-black bg-indigo-100'
+            placeholder='Tasks'
+          />
+          <button
+            type='submit'
+            className='bg-transparent hover:bg-indigo-300 text-indigo-700 font-semibold hover:text-white py-1 px-1 border border-indigo-500 hover:border-transparent rounded-tr rounded-br '
+          >
+            Add Item
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
