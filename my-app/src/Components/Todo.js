c326115b6819333bd3a85540a58753e2bb78ea70
Chai <developer.chaitra@gmail.com>
2023-04-13 10:49:00 +1200

    adding alert and list componemts

--- my-app/src/Components/Todo.js
+++ my-app/src/Components/Todo.js
@@ -1,12 +1,31 @@
 // main class
 
 import React, { useState } from "react";
+import TodoList from "./TodoList";
+import Alert from "./Alert";
 
 const Todo = () => {
   const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className=' w-[90vw] max-w-[35rem] mt-32 mx-auto my-0 rounded shadow-[0_5px_15px_rgba(0,0,0,0.1))] transition-all duration-[0.3s] ease-linear p-8 hover:shadow-[0_5px_15px_rgba(0,0,0,0.5)] bg-violet-100'>
-      <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center'>
+      <form
+        className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-center'
+        onSubmit={handleSubmit}
+      >
+        {alert.show && <Alert />}
         <h3 className='tracking-[0.1rem] capitalize leading-tight text-xl mb-3'>
           To Do List
         </h3>
@@ -24,6 +43,12 @@ const Todo = () => {
           </button>
         </div>
       </form>
+      <div>
+        <TodoList />
+        <button className='bg-transparent capitalize w-40 h-6 grid items-center text-[hsl(360,71%,66%)] text-[0.85rem] tracking-[0.1rem] cursor-pointer transition-all duration-[0.3s] ease-linear mt-5 mx-auto my-0 border-transparent hover:text-white hover:bg-red-500'>
+          Clear Items
+        </button>
+      </div>
     </section>
   );
 };
